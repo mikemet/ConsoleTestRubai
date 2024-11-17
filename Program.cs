2cb31fde457fdfeab0687cdf85c75331702fb085
mikemet <mikemet@ya.ru>
2024-11-17 21:02:44 +0300

    corraction Random method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿Console.WriteLine("Hello!");
-
+﻿
 List<string> lstRubai = [];
 
 FileStream fileStream = new("testRubai.csv", FileMode.Open);
@@ -10,11 +9,21 @@ while (!sr.EndOfStream)
     lstRubai.Add(sr.ReadLine());
 }
 
-int countVal = lstRubai.Count();
+int countVal = lstRubai.Count();    // для справки
 
-Random rnd  = new();
-int rIndex = rnd.Next(lstRubai.Count);
+Random rnd = new();
+int rIndex = rnd.Next(1, lstRubai.Count);
 
 sr.Close();
 fileStream.Close();
 
+// if (rIndex >= lstRubai.Count - 3)
+// {
+//     rIndex = lstRubai.Count - 4;
+// }
+
+string str = lstRubai[rIndex - 1];
+
+Console.WriteLine(countVal);
+Console.WriteLine(rIndex);
+Console.WriteLine(str);
