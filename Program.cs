f869fb13dd01c27dc46e85e82a57a928136f06b4
mikemet <mikemet@ya.ru>
2024-11-17 21:36:23 +0300

    corraction Random

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ while (!sr.EndOfStream)
     lstRubai.Add(sr.ReadLine());
 }
 
-int countVal = lstRubai.Count();    // для справки
+//int countVal = lstRubai.Count();    // для справки
 
 Random rnd = new();
 int rIndex = rnd.Next(1, lstRubai.Count);
@@ -17,13 +17,8 @@ int rIndex = rnd.Next(1, lstRubai.Count);
 sr.Close();
 fileStream.Close();
 
-// if (rIndex >= lstRubai.Count - 3)
-// {
-//     rIndex = lstRubai.Count - 4;
-// }
-
 string str = lstRubai[rIndex - 1];
 
-Console.WriteLine(countVal);
+//Console.WriteLine(countVal);
 Console.WriteLine(rIndex);
 Console.WriteLine(str);
