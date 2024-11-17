cb369ea8dc25cfd52eddbbeee37fb3272ffe6e3e
mikemet <mikemet@ya.ru>
2024-11-17 19:39:59 +0300

    adding code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
 ﻿Console.WriteLine("Hello!");
+
+List<string> lstRubai = [];
+
+FileStream fileStream = new("testRubai.csv", FileMode.Open);
+StreamReader sr = new(fileStream);
+
+while (!sr.EndOfStream)
+{
+    lstRubai.Add(sr.ReadLine());
+}
+
+int countVal = lstRubai.Count();
+
+Random rnd  = new();
+int rIndex = rnd.Next(lstRubai.Count);
+
+sr.Close();
+fileStream.Close();
+
